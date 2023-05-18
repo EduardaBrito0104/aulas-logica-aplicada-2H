26534d48c76de93f6b8c60ab2b3a120bcee71343
EduardaBrito0104 <dudabrito0104@gmail.com>
2023-05-18 11:05:33 -0300

    Entendi!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         // Aula introdutoria de Orientação á Objetos
+        //Declaração de objeto
+        Pessoa adao;
+        // Instaciação do objeto
+        adao = new Pessoa();
+        // Ao mesmo tempo
+        Pessoa qualquer = new Pessoa();
+        // Definir formato do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        qualquer.falar();
+        System.out.println(qualquer.falar("oi"));
 
 
     }
